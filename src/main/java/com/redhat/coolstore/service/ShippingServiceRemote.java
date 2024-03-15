b0b9c137d0a06ecffbe9c1bf2fde01ef95d35baf
Chris May <chmay@redhat.com>
2024-03-15 13:35:51 -0400

    Add a second method to ShippingService
    Added a second method to ShippingService (and exposed the method for
    invocation via remote EJB by adding to ShippingServiceRemote).

--- src/main/java/com/redhat/coolstore/service/ShippingServiceRemote.java
+++ src/main/java/com/redhat/coolstore/service/ShippingServiceRemote.java
@@ -7,4 +7,5 @@ import com.redhat.coolstore.model.ShoppingCart;
 @Remote
 public interface ShippingServiceRemote {
     double calculateShipping(ShoppingCart sc);
+    double calculateShippingInsurance(ShoppingCart sc);
 }
